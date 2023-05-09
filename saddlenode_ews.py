2bc7d5de66bbb9a2444520a2ae2b7f5ca97bda21
Barak Haim <barakiey@gmail.com>
2023-05-09 14:25:48 +0300

    according to ews paper

--- saddlenode_ews.py
+++ saddlenode_ews.py
@@ -7,69 +7,81 @@ from mpl_toolkits.mplot3d import Axes3D
 
 r = 1
 x0 = 0
-y0 = 2
-tt = np.linspace(0, 70, 1000)
+y0 = 0
+epsilon = a = 1
+tt = np.linspace(0, 4, 1000)
 xx = np.linspace(-5, 5, 100)
-
-def dxdt(t, x, r):
-    return r-x**2
-
-def dydt(t, y, r):
-    return -4*y
 
 def dSdt(t, S, epsilon, a):
     x, y = S
+    try:
+        shaper = np.ones_like(y)
+    except:
+        try:
+            shaper = np.ones(len(y))
+        except:
+            shaper = 1
+    
     return [
         y + a*x**2 - x**3,
-        epsilon
+        epsilon * shaper
     ]
 
 sol = solve_ivp(
-    dxdt, t_span=[min(tt), max(tt)], 
-    y0=[x0], t_eval=tt, args=(r,)
+    dSdt, t_span=[min(tt), max(tt)], 
+    y0=[x0, y0], t_eval=tt, args=(epsilon, a)
 )
 
-sol_y = solve_ivp(
-    dydt, t_span=[min(tt), max(tt)], 
-    y0=[y0], t_eval=tt, args=(r,)
-)
+# exit()
 
 fig, axs = plt.subplots(3)
 # Plot solutions to ode's
 sol_plot = axs[0].plot(sol.t, sol.y[0, :], color='b', label='x(t)')
-axs[0].plot(sol_y.t, sol_y.y[0, :], color='orange', label='y(t)')
+axs[0].plot(sol.t, sol.y[1, :], color='orange', label='y(t)')
 axs[0].set_xlabel('time')
-axs[0].set_ylabel(r'x', rotation=0)
-axs[0].set_xlim([0, 2])
-axs[0].set_ylim([-2, 2])
+axs[0].set_ylabel(r'x/y', rotation=0)
+axs[0].set_xlim([0, 3])
+axs[0].set_ylim(
+    [
+        min(sol.y[0, :]), 
+        max(sol.y[0, :])
+])
 axs[0].grid()
 axs[0].legend()
 
+
 # Plot phase space of derivatives
-phase_space = axs[1].plot(xx, dxdt(sol.t,xx,r), color='b', label='x')
-axs[1].plot(xx, dydt(sol_y.t,xx,r), color='orange', label='y')
+X_dot, Y_dot = dSdt(sol.t,[xx,xx], epsilon, a)
+
+phase_space = axs[1].plot(xx, X_dot, color='b', label='x')
+axs[1].plot(xx, Y_dot, color='orange', label='y')
 scat = axs[1].scatter([], [], c='black')
 
 axs[1].grid()
 axs[1].set_xlabel('x')
 axs[1].set_ylabel(r'$\dot{x}$', rotation=0)
-axs[1].set_ylim([-1.1,1.1])
+axs[1].set_ylim([-2,2])
+axs[1].set_xlim([min(xx),max(xx)])
 axs[1].legend()
 
 # Streamlines of X - Y space 
 w = 5
 Y, X = np.mgrid[-w:w:1000j, -w:w:1000j]
-U = dxdt(tt, X, r)
-V = dydt(tt, Y, r)
+U, V = dSdt(sol.t,[X,Y], epsilon, a)
+print(X.shape, Y.shape, U.shape, V.shape)
 stream = axs[2].streamplot(X, Y, U, V, density = 1)
-axs[2].set_ylim([-1, 1])
+axs[2].set_ylim([-5, 5])
 axs[2].set_xlim([-1*w, w])
-left_atractor_line = axs[2].axvline(-1*r, c='black', ls='--')
-right_atractor_line = axs[2].axvline(r, c='black', ls='--')
-axs[2].axhline(0, c='black')
-left_atractor_scat = axs[2].scatter((-1 * r),0, facecolors='none', edgecolors='black')
-right_atractor_scat = axs[2].scatter(r,0, c='black')
-axs[2].set_aspect('equal', adjustable='box')
+
+# left_atractor_line = axs[2].axvline(-1*r, c='black', ls='--')
+# right_atractor_line = axs[2].axvline(r, c='black', ls='--')
+# axs[2].axhline(0, c='black')
+# left_atractor_scat = axs[2].scatter((-1 * r),0, facecolors='none', edgecolors='black')
+# right_atractor_scat = axs[2].scatter(r,0, c='black')
+# axs[2].set_aspect('equal', adjustable='box')
+
+plt.show()
+exit()
 
 tot_frames = 40
 def update(frame):
@@ -120,5 +132,5 @@ def update(frame):
 
 # TODO: use init_func for animation
 ani = animation.FuncAnimation(fig=fig, func=update, frames=tot_frames, interval=50)
-# ani.save('./animation.gif', writer='imagemagick', fps=60)
-plt.show()
+ani.save('./animation.gif', writer='imagemagick', fps=60)
+# plt.show()
