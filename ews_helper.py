8810b2ff9f520b6c4fdc95d8d6ae6a0b5489cb84
Barrack Ha <barakiey@gmail.com>
2023-05-29 08:21:55 +0300

    ar1s as ndarray

--- ews_helper.py
+++ ews_helper.py
@@ -5,14 +5,14 @@ def get_ews(time, arr, win_size=21, offset=1):
     @Params: time, arr, win_size, offset
     Returns: dict of block_idxs and ar1s
     """ 
-    ar1s = []
     block_idxs = np.arange(time.shape[0]-win_size, step=offset)
-    
+    ar1s = np.zeros_like(block_idxs, dtype=np.float64)
+
     for i in block_idxs:
         try:
             lag0 = arr[i: i+win_size]
             lag1 = arr[i+offset: i+offset+win_size]
-            ar1s.append(np.corrcoef(lag0, lag1)[0, 1])
+            ar1s[i] = np.corrcoef(lag0, lag1)[0, 1]
         except:
             print(f'error in {i}')
 
