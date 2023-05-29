9d041a8f27f6bfe4c1e491b34d9653f5b4f226fb
Barrack Ha <barakiey@gmail.com>
2023-05-29 10:48:04 +0300

    refactored

--- hopf_maruyama.py
+++ hopf_maruyama.py
@@ -3,31 +3,17 @@ import matplotlib.pyplot as plt
 from matplotlib import animation as anim, rc
 from matplotlib.collections import LineCollection
 from scipy.stats import linregress
+from ews_helper import get_ews, itoEulerMaruyama
 
 def annotate_axes(ax, text, fontsize=12):
     ax.text(0.5, 0.5, text, transform=ax.transAxes,
             ha="right", va="top", fontsize=fontsize, color="darkgrey")
 
+def set_axes_title(ax, text, fontsize=12):
+    ax.title.set_text(text)
+    ax.title.fontsize = fontsize
 
-def itoEulerMaruyama(model, y0, time, noise, args=None, save_derivative=False):
-    ret_val = np.zeros((len(time), len(y0)))
-    noise = np.array(noise)
-    y0 = np.array(y0)
-    ret_val[0, :] = y0
-    dt = time[1] - time[0]
-    derivatives = np.zeros((len(time), len(y0)))
-    for i in range(1, len(time)):
-        derivatives[i-1, :] = np.array(
-            model(ret_val[i - 1, :], time[i], *args) 
-                if args else model(ret_val[i - 1, :], time[i])
-        ) 
-        ret_val[i, :] = ret_val[i - 1, :] + \
-                        derivatives[i-1, :] * dt + \
-                        noise*np.random.normal(0,np.sqrt(dt),ret_val.shape[1])
-        
-    return ret_val if not save_derivative else (ret_val,derivatives)
-
-def hopf_model(S, t, epsilon, omega, b):
+def hopf_model(t, S, epsilon, omega, b):
     """S = [dr/dt, dtheta/dt, dmu/dt]"""
     r, theta, mu = S
     try:
@@ -44,6 +30,47 @@ def hopf_model(S, t, epsilon, omega, b):
         epsilon * shaper
     ]
 
+def calc_and_plot_hopf_maruyama(
+        ax, time, r_0, theta_0, mu_0, epsilon, noise, b, 
+        save_derivative=True, plot_strech=1.2, 
+        polar_to_cartisian=True, color='b', label='Hopf'):
+    """
+    Plot the hopf
+    Returns t_star, ax, results, derivatives
+    """
+
+    results, derivatives = itoEulerMaruyama(
+        hopf_model,
+        y0=[r_0, theta_0, mu_0],
+        time=time,noise=[0, noise, 0],
+        args=(epsilon, omega, b), save_derivative=save_derivative
+    )
+
+    rs, thetas, mus = results[:,0], results[:,1], results[:,2]
+    if polar_to_cartisian:
+        x = rs*np.cos(thetas)
+        y = rs*np.sin(thetas)
+    else:
+        x = rs
+        y = thetas
+
+    slope,intercept,_,_,_ = linregress(time, mus)
+    # mu=0=intercept+slope*t iff t=-intercept/slope
+    t_star = -intercept/slope
+
+    w = plot_strech
+    ax.plot(x,y, label=label, c=color)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_xlim(x.min()*w, x.max()*w)
+    ax.set_ylim(y.min()*w, y.max()*w)
+    ax.scatter(x[0], y[0], c='g', s=50, label='start')
+    ax.scatter(x[-1], y[-1], c='r', s=50, label='end')
+    ax.legend()
+
+    return t_star, ax, results, derivatives
+
+
 
 if __name__ == '__main__':
 
@@ -54,87 +81,105 @@ if __name__ == '__main__':
     mu_0 = -2.0
     epsilon = 0.01
     sigma = 0.9
-    time = np.linspace(0, 410, 50000)
+    time = np.linspace(0, 510, 50000)
+    ews_win_size, ews_offset = 21, 1
 
     # fig, axs = plt.subplots(1,2)
     fig = plt.figure(figsize=(16, 8))
-    spec = fig.add_gridspec(2, 2)
-    w = 1.2
+    spec = fig.add_gridspec(3, 2)
 
-    nosiy_results, nosiy_derivatives = itoEulerMaruyama(
-        hopf_model,
-        y0=[r_0, theta_0, mu_0],
-        time=time,noise=[0, sigma, 0],
-        args=(epsilon, omega, b),save_derivative=True
-    )
-    nosiy_rs, nosiy_thetas = nosiy_results[:,0], nosiy_results[:,1]
-    nosiy_x = nosiy_rs*np.cos(nosiy_thetas)
-    nosiy_y = nosiy_rs*np.sin(nosiy_thetas)
-    
+    # Hopf with noise
     ax00 = fig.add_subplot(spec[0, 0])
-    annotate_axes(ax00, 'with noise')
-    ax00.plot(nosiy_x,nosiy_y, label='with noise')
-    ax00.set_xlim(nosiy_x.min()*w, nosiy_x.max()*w)
-    ax00.set_ylim(nosiy_y.min()*w, nosiy_y.max()*w)
-    ax00.scatter(nosiy_x[0], nosiy_y[0], c='g', s=50, label='start')
-    ax00.scatter(nosiy_x[-1], nosiy_y[-1], c='r', s=50, label='end')
-    ax00.legend()
-
-    # Without noise
-    results,derivatives = itoEulerMaruyama(
-        hopf_model,
-        y0=[r_0, theta_0, mu_0],
-        time=time,noise=[0,0,0],
-        args=(epsilon, omega, b),save_derivative=True
+    noisy_hopf = calc_and_plot_hopf_maruyama(
+        ax00, time, r_0, theta_0, mu_0, epsilon, noise=sigma, b=b,
+        label=None, color='b'
     )
-    rs, thetas=results[:,0],results[:,1]
-    x = rs*np.cos(thetas)
-    y = rs*np.sin(thetas)
-    
-    ax01 = fig.add_subplot(spec[0, 1])
-    annotate_axes(ax01, 'without noise')
-    ax01.plot(x,y, label='without noise', c='orange')
-    ax01.set_xlim(x.min()*w,x.max()*w)
-    ax01.set_ylim(y.min()*w,y.max()*w)
-    ax01.scatter(x[0], y[0], c='g', s=50, label='start')
-    ax01.scatter(x[-1], y[-1], c='r', s=50, label='end')
-    ax01.legend()
+    set_axes_title(ax00, 'Hopf With noise', fontsize=8)
 
-    # ax1 = fig.add_subplot(spec[1, :])
-    # annotate_axes(ax1, 'SOl as a function of time')
-    # for i, name in enumerate(['r','theta','mu']):
-    #     ax1.plot(time,results[:,i], label=name)
-    # ax1.plot(time,x, label='x(t)')
-    # ax1.plot(time,y, label='y(t)', linestyle='-.')
-    # ax1.set_xlim(time.min(),time.max())
-    # ax1.grid()
-    # ax1.legend()
-    # print(np.allclose(x,y, rtol=1e-1, atol=1))
-    # print(x[:5], y[:5])
+    # Hopf without noise
+    ax01 = fig.add_subplot(spec[0, 1])
+    no_noise = calc_and_plot_hopf_maruyama(
+        ax01, time, r_0, theta_0, mu_0, epsilon, noise=0, b=b,
+        label=None, color='orange'
+    )
+    set_axes_title(ax01, 'Hopf Without noise', fontsize=8)
 
     ax1 = fig.add_subplot(spec[1, :])
     ax1.set_facecolor(plt.cm.gray(.95))
-    win_size = 21 
-    for i in range (1, 2):
-        ar1s = []
-        noisy_ar1s = []
-        offset = i
-        block_idxs = np.arange(time.shape[0]-win_size, step=offset)
-        
-        for i in block_idxs:
-            try:
-                lag0 = thetas[i: i+win_size]
-                lag1 = thetas[i+offset: i+offset+win_size]
-                n_lag0 = nosiy_thetas[i: i+win_size]
-                n_lag1 = nosiy_thetas[i+offset: i+offset+win_size]
-                ar1s.append(np.corrcoef(lag0, lag1)[0, 1])
-                noisy_ar1s.append(np.corrcoef(n_lag0, n_lag1)[0, 1])
-            except Exception as e:
-                print(f'error in {i}\n{e}')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('AR1')
+
+    """
+    # EWS of Hopf with noise
+    block_idxs, noisy_ar1s = map(
+        get_ews(
+            time, noisy_hopf[2][:,1], 
+            win_size=ews_win_size, offset=ews_offset
+        ).get,
+        ['block_idxs', 'ar1s']
+    )
+    ax1.plot(
+        time[block_idxs[:len(noisy_ar1s)]], noisy_ar1s, label=f'Noised Thetas'
+    )
+
+    # EWS of Hopf without noise
+    ar1s = get_ews(
+        time, no_noise[2][:,1], win_size=ews_win_size, offset=ews_offset
+    )['ar1s']    
+    ax1.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'No Noise Thetas')
     
-    print(block_idxs[:len(ar1s)].shape, len(ar1s))
-    ax1.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'Normal Form')
-    ax1.plot(time[block_idxs[:len(ar1s)]], noisy_ar1s, label=f'With Noise')
+    # ax1.axvline(no_noise[0], color='k', linestyle='--', alpha=0.5, label='t*')
+    ax1.axvline(noisy_hopf[0], color='k', linestyle='--', alpha=0.5, label='noisy_t*')
     ax1.legend()
+    set_axes_title(ax1, 'AR1 of Thetas', fontsize=8)
+    """
+
+    ax2 = fig.add_subplot(spec[2, :], sharex=ax1)
+    ax2.set_facecolor(plt.cm.gray(.95))
+    # ax2.set_xlabel('Time')
+    ax1.set_ylabel('AR1')
+    # for i in [0,2]:
+    #     ax2.plot(time, no_noise[2][:,i], label=f'results[:, {i}]')
+    # ax2.axvline(no_noise[0], color='k', linestyle='--', alpha=0.5, label='t*')
+    # ax2.legend()
+
+    x = no_noise[2][:,0]*np.cos(no_noise[2][:,1])
+    y = no_noise[2][:,0]*np.sin(no_noise[2][:,1])
+    noisy_x = noisy_hopf[2][:,0]*np.cos(noisy_hopf[2][:,1])
+    noisy_y = noisy_hopf[2][:,0]*np.sin(noisy_hopf[2][:,1])
+
+
+    block_idxs, ar1s = map(
+        get_ews(
+            time, x, 
+            win_size=ews_win_size, offset=ews_offset
+        ).get,
+        ['block_idxs', 'ar1s']
+    )  
+    ax1.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'x')
+
+    ar1s = get_ews(
+        time, y, win_size=ews_win_size, offset=ews_offset
+    )['ar1s']    
+    ax1.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'y')
+
+    block_idxs, ar1s = map(
+        get_ews(
+            time, noisy_x, 
+            win_size=ews_win_size, offset=ews_offset
+        ).get,
+        ['block_idxs', 'ar1s']
+    )  
+    ax2.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'noisy_x')
+
+    ar1s = get_ews(
+        time, noisy_y, win_size=ews_win_size, offset=ews_offset
+    )['ar1s']    
+    ax2.plot(time[block_idxs[:len(ar1s)]], ar1s, label=f'noisy_y')
+    ax2.legend()
         
     plt.show()
+
+# with noise / without noise
+# AR1, decay time, var
+# [x, y], [r, theta, mu], PCA
