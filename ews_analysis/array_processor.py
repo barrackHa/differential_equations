62905333c6aa2e535bc9e68c13b15ea16200083e
Barak Haim <barakiey@gmail.com>
2023-08-13 16:23:01 +0300

    test stride_tricks

--- ews_analysis/array_processor.py
+++ ews_analysis/array_processor.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 
 #%% 
 
-n = sim_time_arr_len = 30
-sim_time = np.linspace(0, 30, sim_time_arr_len)
-win_size=9
+n = sim_time_arr_len = 70
+# sim_time = np.linspace(0, 30, sim_time_arr_len)
+sim_time = np.arange(0, sim_time_arr_len)
+
+win_size=11
 offset=3
 
 #%%
@@ -18,4 +20,11 @@ for i in block_idxs:
 
 
 
+# %%
+
+arr = np.arange(10)
+sw_view = np.lib.stride_tricks.sliding_window_view(
+    arr, 3)
+# %%
+s = np.lib.stride_tricks.as_strided(arr, (arr.size-4, 3), arr.strides*4)
 # %%
