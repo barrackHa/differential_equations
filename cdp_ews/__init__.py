549c1bfcafeeb2b934388729c404e0f7fe914bca
Barrack Ha <barakiey@gmail.com>
2023-09-18 15:22:06 +0300

    fixed init

---
+++ cdp_ews/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+name = 'cdp_ews'
+
+# Import relevant modules
+from . import core
+from . import helpers
+from . import models
+
+# # Import specific classes and functions
+# from .core import TimeSeries
+# from .core import MultiTimeSeries
